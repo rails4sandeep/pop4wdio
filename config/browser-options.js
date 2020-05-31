351168df0c0331e7a5377b9c012cee47a0d3a612
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-31 16:11:36 +1000

    Change browser-options.js to use chrome headless

--- config/browser-options.js
+++ config/browser-options.js
@@ -2,8 +2,14 @@ browserOptions =
 {
     logLevel: 'silent',
         capabilities: {
-    browserName: 'chrome'
-}
+            automationProtocol: 'devtools',
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                // to run chrome headless the following flags are required
+                // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+                args: ['--headless', '--disable-gpu']
+            }
+    }
 };
 
 module.exports = browserOptions;
