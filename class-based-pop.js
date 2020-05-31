37d13aa98fe610f07865507d2870d828277df567
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-31 15:08:11 +1000

    Update class-based-pop.js

--- class-based-pop.js
+++ class-based-pop.js
@@ -1,16 +1,10 @@
-const { remote } = require('webdriverio');
-
-
+const Session = require('./pop/session');
+const session = new Session();
 const DuckDuckGo = require('./pop/duck-duck-go-class');
 const duckDuckGo = new DuckDuckGo();
 
 const wdioExample = async () => {
-    const browser = await remote({
-        logLevel: 'trace',
-        capabilities: {
-            browserName: 'chrome'
-        }
-    });
+    const browser = await session.newSession();
     const Page = require('./pop/page');
     const page = new Page(browser);
     await page.goto('https://duckduckgo.com');
@@ -18,7 +12,7 @@ const wdioExample = async () => {
     await page.clickElement(duckDuckGo.searchButton);
     const title = await page.getTitle();
     console.log(title); // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
-    await page.close();
+    await session.closeSession(browser);
 } ;
 
 wdioExample();
