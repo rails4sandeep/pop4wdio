297d47ac5865d2a14e6b9227e52be89399226d3f
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-31 15:20:38 +1000

    Add a jest test example

---
+++ test/duck-duck-go.test.js
@@ -0,0 +1,25 @@
+const Page = require('../pop/page');
+const Session = require('../pop/session');
+const session = new Session();
+let browser, page;
+const duckDuckGo = require('../pop/duck-duck-go-obj');
+
+describe('Duck Duck Go', () => {
+
+    beforeEach(async () => {
+        browser = await session.newSession();
+        page = new Page(browser);
+    });
+
+    afterEach(async () => {
+        await session.closeSession(browser);
+    });
+
+    it('should return search results when we search', async () => {
+        await page.goto('https://duckduckgo.com');
+        await page.enterText(duckDuckGo.searchInput,'WebdriverIO');
+        await page.clickElement(duckDuckGo.searchButton);
+        const title = await page.getTitle();
+        expect(title).not.toBeNaN();
+    });
+});
