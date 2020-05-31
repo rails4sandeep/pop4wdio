97bd8c13c000782780afccf4373d58a94694605a
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-31 18:31:19 +1000

    Update browser-options.js

--- config/browser-options.js
+++ config/browser-options.js
@@ -1,12 +1,13 @@
 browserOptions =
 {
-    logLevel: 'silent',
+    logLevel: 'debug',
         capabilities: {
             browserName: 'chrome',
+            baseUrl: 'http://localhost',
             'goog:chromeOptions': {
                 // to run chrome headless the following flags are required
                 // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-                args: ['--headless', '--disable-gpu']
+
             }
     }
 };
